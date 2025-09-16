f34c0d3752c16eaf1eb543c2f6f0ae61c0e541e4
CaregnatoGianluca <gianluca.caregnato@gmail.com>
2025-09-16 18:41:44 +0200

    cmake

---
+++ build/CMakeFiles/opencv_hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_hello.dir/src/main.cpp.o"
+  "CMakeFiles/opencv_hello.dir/src/main.cpp.o.d"
+  "opencv_hello"
+  "opencv_hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
