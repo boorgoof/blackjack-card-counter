c575d4a128ca5806bd000f7d542f13be9318663b
EndlessDoomsayer <matt.bno.tv@gmail.com>
2025-09-29 19:04:04 +0200

    removed type erasure maps as they are not needed, updated class diagram

--- code/include/FeatureContainer.h
+++ code/include/FeatureContainer.h
@@ -31,32 +31,4 @@ private:
     std::map<Card_Type, feature_type> descriptors_;
 };
 
-// Type-erased interface
-class IDescriptorMap {
-public:
-    virtual ~IDescriptorMap() = default;
-    virtual cv::InputArray get(Card_Type type) const = 0;
-    virtual std::size_t size() const = 0;
-};
-
-// Concrete typed holder
-template<typename T>
-class DescriptorMapHolder : public IDescriptorMap {
-public:
-    explicit DescriptorMapHolder(const std::map<Card_Type, T>* map) : map_(map) {}
-
-    cv::InputArray get(Card_Type type) const override {
-        auto it = map_->find(type);
-        if (it == map_->end()) {
-            return cv::noArray();
-        }
-        return cv::InputArray(it->second);
-    }
-
-    std::size_t size() const override { return map_->size(); }
-
-private:
-    const std::map<Card_Type, T>* map_;
-};
-
 #endif // FEATURECONTAINER_H
