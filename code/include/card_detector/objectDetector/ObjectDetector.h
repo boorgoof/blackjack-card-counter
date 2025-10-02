b473160105c6b5fcd97dae238fad60cf79fa6560
Boorgoof <federico.31mene@icloud.com>
2025-10-02 16:21:08 +0200

    some edits

--- code/include/card_detector/objectDetector/ObjectDetector.h
+++ code/include/card_detector/objectDetector/ObjectDetector.h
@@ -17,9 +17,10 @@ class ObjectDetector{
         ObjectDetector() = default;
 
         ObjectDetector(ObjectDetector&&) = delete;
-        ObjectDetector& operator=(ObjectDetector&&) = delete;
+        ObjectDetector& operator=(ObjectDetector&&) = delete; //TODO
         virtual ~ObjectDetector() = 0;
 
+        
         /**
          * @brief detect objects in a scene image
          * @param src_img the scene image to detect objects from
@@ -27,14 +28,6 @@ class ObjectDetector{
          */
         virtual void detect_objects(const cv::Mat& src_img, std::vector<Label>& out_labels) = 0;
 
-        /**
-         * @brief detect objects in a whole dataset: detect objects in each image of the dataset and stores the results in a map.
-         * @param dataset the dataset to detect objects from . Remember a dataset manages a single object's test images
-         * @param predicted_items the output map of predicted items
-         * @return the number of detected objects
-         */
-        const size_t detect_object_whole_dataset(const Dataset& dataset, std::map<std::string, std::vector<Label>>& predicted_items);
-        
         void set_method_name(const std::string& method_name) { this->method_name = method_name; }
         const std::string& get_method_name() const { return this->method_name; }
         
