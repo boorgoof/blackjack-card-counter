863de7def212c3d21d80b33589ac634e06522635
endlessDoomsayer <matt.bno.tv@gmail.com>
2025-11-15 16:13:53 +0100

    image visualization utils moved from main to Utils (resize, show labels and BBoxes)

--- code/include/Utils.h
+++ code/include/Utils.h
@@ -19,9 +19,38 @@ namespace Utils{
         std::string longestCommonPath(const std::string& path1_str, const std::string& path2_str);
         std::filesystem::path longestCommonPath(const std::filesystem::path& path1, const std::filesystem::path& path2);
     }
-    
+
+    /**
+     * @brief functions for visualizing data and progress.
+     */
     namespace Visualization{
+        /**
+         * @brief Prints a progress bar to the console.
+         * @param progress a float between 0 and 1 indicating the progress
+         * @param barwidth the width of the progress bar
+         * @param prefix a string to print before the progress bar
+         * @param suffix a string to print after the progress bar
+         */
         void printProgressBar(float progress, size_t barwidth, const std::string& prefix = "", const std::string& suffix = "");
+        void showImage(cv::Mat& image, const std::string& window_name = "Image", const int time = 0, const float resize_factor = 1.0);
+        void showImage(cv::Mat& image, const std::string& window_name = "Image", const int time = 0, const cv::Size& size = cv::Size());
+        /**
+         * @brief draws the provided labels directly on the image.
+         * @param image the image to draw the labels on
+         * @param labels the labels to draw
+         * @param box_color the color of the bounding boxes
+         * @param text_color the color of the text
+         */
+        void printLabelsOnImage(cv::Mat& image, const std::vector<Label>& labels, const cv::Scalar& box_color, const cv::Scalar& text_color);
+        /**
+         * @brief Creates a clone of the input image and shows it with ground truth and predicted labels drawn on it.
+         * @param image the image to show
+         * @param ground_truth_labels the ground truth labels to draw
+         * @param predicted_labels the predicted labels to draw
+         * @param window_name the name of the window to show the image in
+         */
+        void showImageWithLabels(const cv::Mat& image, const cv::Size& size = cv::Size(800, 600), const std::vector<Label>& ground_truth_labels = {}, const std::vector<Label>& predicted_labels = {}, const cv::Scalar& gt_color = cv::Scalar(0,255,0), const cv::Scalar& pred_color = cv::Scalar(255,0,0), const int time=0, const std::string& window_name = "Image with Labels");
+        void showImageWithLabels(const cv::Mat& image, const float& resize_factor = 1, const std::vector<Label>& ground_truth_labels = {}, const std::vector<Label>& predicted_labels = {}, const cv::Scalar& gt_color = cv::Scalar(0,255,0), const cv::Scalar& pred_color = cv::Scalar(255,0,0), const int time=0, const std::string& window_name = "Image with Labels");
     }
 
     /**
